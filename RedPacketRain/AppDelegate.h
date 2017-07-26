a8b2414f5c76c5759a42d7240cfe00b0c9791a3a
zhangzy <zhang434586301@163.com>
2017-07-26 18:43:34 +0800

    提交

---
+++ RedPacketRain/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RedPacketRain
+//
+//  Created by zhangzy on 2017/7/26.
+//  Copyright © 2017年 zhangzy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
